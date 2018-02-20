43e548ce1e0711a56da2f1336f617f8758115213
bhargavaukkalam <34873447+bhargavaukkalam@users.noreply.github.com>
2018-02-20 03:15:00 -0500

    Update 11.containerWithMostWater.java

--- 11.containerWithMostWater.java
+++ 11.containerWithMostWater.java
@@ -1,7 +1,13 @@
-import java.util.Scanner;
+/*
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
-public class MaxAreaOfIsland {
-	public int maxAreaOfIsland(int[] height){
+Note: You may not slant the container and n is at least 2.
+
+import java.util.Scanner;
+*/
+
+public class MaxArea {
+	public int maxAreaWater(int[] height){
 		int maxArea=0;
 		int start=0;
 		int end=height.length-1;
@@ -22,14 +28,14 @@ public class MaxAreaOfIsland {
 	
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
-		MaxAreaOfIsland objmax=new MaxAreaOfIsland();
+		MaxArea objmax=new MaxArea();
 		int[] inArray=new int[9];
 		System.out.println("Enter input :");
 		for(int i=0;i<inArray.length;i++){
 			inArray[i]=input.nextInt();
 		}
 		
-		int out =objmax.maxAreaOfIsland(inArray);
+		int out =objmax.maxAreaWater(inArray);
 		System.out.println("Max area of island is :");
 		System.out.println(out);
 		
