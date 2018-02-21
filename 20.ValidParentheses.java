67e1cd020ed56ec2d70b08923e0a2f36efee08f0
bhargavaukkalam <34873447+bhargavaukkalam@users.noreply.github.com>
2018-02-21 03:57:37 -0500

    Update 20.ValidParentheses.java

--- 20.ValidParentheses.java
+++ 20.ValidParentheses.java
@@ -1,3 +1,9 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
 import java.util.Scanner;
 import java.util.Stack;
 
