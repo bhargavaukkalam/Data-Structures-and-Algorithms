55e613c39e9df4a443c71c76c7ce3094d1133aa4
bhargavaukkalam <34873447+bhargavaukkalam@users.noreply.github.com>
2018-02-23 00:57:20 -0500

    Add files via upload

---
+++ 66.PlusOne.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PlusOne {
+	 public int[] plusOne(int[] digits) {
+	        int n=digits.length;
+	        for(int i=n-1;i>=0;i--){
+	            if(digits[i]<9){
+	                digits[i]++;
+	                return digits;
+	            }
+	            digits[i]=0;
+	        }
+	        
+	        int[] newNumber=new int[n+1];
+	        newNumber[0]=1;
+	        
+	        return newNumber;
+	    }
+	 
+	 public static void main(String[] args) {
+		PlusOne obj=new PlusOne();
+		Scanner input=new Scanner(System.in);
+		int[] array=new int[5];
+		System.out.println("Enter array of digits : ");
+		for(int i=0;i<array.length;i++){
+			array[i]=input.nextInt();
+		}
+		obj.plusOne(array);
+		System.out.println("Output array is : ");
+		for(int i=0;i<array.length;i++){
+			System.out.print(array[i]+" ");
+		}
+	}
+}
