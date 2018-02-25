644de7b2e4cde1f18464538db0944df69cf56e81
bhargavaukkalam <34873447+bhargavaukkalam@users.noreply.github.com>
2018-02-24 20:32:40 -0500

    Add files via upload

---
+++ 104.MaxDepthBinaryTree.java
@@ -0,0 +1,50 @@
+class TreeNode3{
+	int val;
+	TreeNode3 left;
+	TreeNode3 right;
+	public TreeNode3(int x) {
+		val=x;
+	}
+	
+}
+public class MaxDepthBinaryTree {
+	TreeNode3 root;
+
+	public MaxDepthBinaryTree() {
+		root=null;
+	}
+	
+	public int maxDepth(TreeNode3 root){
+		if(root==null){
+			return 0;
+		}
+		int left=maxDepth(root.left);
+		int right=maxDepth(root.right);
+		
+		return 1+Math.max(left, right);
+	}
+	
+	public void inOrder(TreeNode3 node){
+		if(node==null){
+			return;
+		}
+		inOrder(node.left);
+		System.out.print(node.val+" ");
+		inOrder(node.right);
+		
+	}
+	
+	public static void main(String[] args) {
+		MaxDepthBinaryTree btree=new MaxDepthBinaryTree();
+		btree.root=new TreeNode3(3);
+		btree.root.left=new TreeNode3(2);
+		btree.root.right=new TreeNode3(7);
+		btree.root.left.left=new TreeNode3(1);
+		btree.root.right.left=new TreeNode3(6);
+		btree.root.right.left.left=new TreeNode3(5);
+		btree.inOrder(btree.root);
+		System.out.println();
+		int output=btree.maxDepth(btree.root);
+		System.out.println("Maximum depth is : "+ output);
+	}
+}
